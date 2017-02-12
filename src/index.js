2f14edff6f70314b390c98a7acb644caeab56dd0
Zenaro <572001239@qq.com>
2017-02-12 22:42:44 +0800

    router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './component/Index/App.js';
+import About from './component/Index/About.js';
 
 ReactDOM.render(
 	<App />,
