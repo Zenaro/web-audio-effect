ea9feb421bc296ae3bd7afe8dbec025aaaaa50f7
Zenaro <572001239@qq.com>
2017-02-25 18:11:16 +0800

    audio-check

--- src/Lib/context/Recorder.js
+++ src/Lib/context/Recorder.js
@@ -14,24 +14,27 @@ module.exports = {
 			navigator.getUserMedia({
 				audio: true
 			}, (stream) => {
-				
+
 				this.mediaRecorder = new window.MediaRecorder(stream);
 				this.mediaRecorder.start();
 				this.mediaRecorder.ondataavailable = (e) => {
-					this.chunks.push(e.data);	// store the recorder
+					this.chunks.push(e.data); // store the recorder
 				};
-				this.mediaRecorder.onstop = (e) => {	// over the recorder
+				this.mediaRecorder.onstop = (e) => { // over the recorder
 					AudioCtx.audio.pause();
 					let clipName = prompt('给刚才的录音起个名字吧:');
 					let blob = new Blob(this.chunks, {
 						'type': 'audio/ogg; codecs=opus'
 					})
 					let audioURL = window.URL.createObjectURL(blob);
-					console.log(audioURL);
-					filesAdd(e, {'file': null, 'title': clipName, 'src': audioURL});
+					filesAdd(e, {
+						'file': null,
+						'title': clipName,
+						'src': audioURL
+					});
 					this.source.disconnect(0);
 				}
-				
+
 				this.source = AudioCtx.audioCtx.createMediaStreamSource(stream);
 				let recorder = AudioCtx.audioCtx.createScriptProcessor(1024, 1, 1);
 				this.source.connect(recorder);
